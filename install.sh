374fd67995133cbf273cda98376ab94bdcc1a2e8
Saiid El Hajj Chehade <saidhajjchehade@gmail.com>
2022-11-30 02:54:30 +0200

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,4 @@
+pip install classifier/requirements.txt;
+cp livedetection.py ~/kdd99_feature_extractor/livedetection.py;
+cp classifier ~/kdd99_feature_extractor/classifier;
+cp run-detect.sh ~/kdd99_feature_extractor/run-detect.sh;
